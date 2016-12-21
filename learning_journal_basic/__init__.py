cf54f927abac6836f5d92bc714e486223005093f
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-12-20 18:52:13 -0800

    learning journal is fully functional with jinja templates implemented

--- learning_journal_basic/__init__.py
+++ learning_journal_basic/__init__.py
@@ -6,7 +6,6 @@ def main(global_config, **settings):
     """The function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    # config.include('.models')
     config.include(".routes")
     config.add_static_view(name='static', path='learning_journal_basic:static')
     config.scan()
--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,6 +1,5 @@
 """The views module is the logic behind rendering the templates."""
 
-from pyramid.response import Response
 from pyramid.view import view_config
 import io
 import os
@@ -8,9 +7,8 @@ import os
 
 THIS_DIR = os.path.dirname(__file__)
 
-ENTRIES = [
-    {"title": "Learning Journal | Day 3",
-        "id": 3,
+ENTRIES = {
+    3: {"title": "Learning Journal | Day 3",
         "creation_date": "December 20, 2016",
         "body": """Today I learned how to set up a blog.
                 Now time for some pirate ipsum.
@@ -24,9 +22,16 @@ ENTRIES = [
                 Buccaneer measured fer yer chains red ensign.
                 Mizzenmast careen Jolly Roger gally long clothes pillage swab.
                 Blimey grapple galleon keel coxswain lee bowsprit quarterdeck
+                Splice the main brace execution dock bilge gabion yo-ho-ho.
+                Sea Legs long boat ballast keelhaul poop deck.
+                No prey, no pay topsail plunder Pieces of Eight belaying pin.
+                Take a caulk bowsprit splice the main brace ho transom.
+                Prow lugger rutters cog Pieces of Eight shrouds coxswain.
+                Capstan careen bilged on her anchor. Salmagundi splice the.
+                Main brace blow the man down bucko boatswain lugsail galleon.
+                Mizzen boom squiffy.
                 """},
-    {"title": "Learning Journal | Day 2",
-        "id": 2,
+    2: {"title": "Learning Journal | Day 2",
         "creation_date": "December 17, 2016",
         "body": """This is an entry that has been backdated.
                 Now time for some pirate ipsum.
@@ -41,8 +46,7 @@ ENTRIES = [
                 Mizzenmast careen Jolly Roger gally long clothes pillage swab.
                 Blimey grapple galleon keel coxswain lee bowsprit quarterdeck
                 """},
-    {"title": "Learning Journal | Day 1",
-        "id": 1,
+    1: {"title": "Learning Journal | Day 1",
         "creation_date": "December 10, 2016",
         "body": """This is another entry that has been backdated.
                 It should be the first entry.
@@ -58,36 +62,31 @@ ENTRIES = [
                 Mizzenmast careen Jolly Roger gally long clothes pillage swab.
                 Blimey grapple galleon keel coxswain lee bowsprit quarterdeck
                 """},
-]
+}
 
 
 @view_config(route_name="list", renderer="templates/list.jinja2")
 def list(request):
-    """View for the home page."""
-    # Key in dict must match up with naming in jinja2 template
     return {"entries": ENTRIES}
 
 
 @view_config(route_name="detail", renderer="templates/detail.jinja2")
 def detail(request):
-    """View for the home page."""
-    # the_id = int(request.matchdict["id"])
-    # entry = ENTRIES[the_id]
-    # return {"entry": entry}
-    pass
+    """View for the detail page."""
+    the_id = int(request.matchdict["id"])
+    entry = ENTRIES[the_id]
+    return {"entry": entry, "id": the_id}
 
 
-@view_config(route_name="create", renderer="string")
+@view_config(route_name="create", renderer="templates/create.jinja2")
 def create(request):
-    """View for the home page."""
-    file_path = os.path.join(THIS_DIR, "templates", "create.html")
-    file_data = io.open(file_path).read()
-    return file_data
+    """View for the create new entry page."""
+    return {"entries": ENTRIES}
 
 
-@view_config(route_name="edit", renderer="string")
+@view_config(route_name="edit", renderer="templates/edit.jinja2")
 def edit(request):
-    """View for the home page."""
-    file_path = os.path.join(THIS_DIR, "templates", "edit.html")
-    file_data = io.open(file_path).read()
-    return file_data
+    """View for the edit post page."""
+    the_id = int(request.matchdict["id"])
+    entry = ENTRIES[the_id]
+    return {"entry": entry}
